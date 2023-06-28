ce64e5fc48ddd903a893e61118c4a53fc5e20f3e
lawrahkonwea <lawrahcollections@gmail.com>
2023-06-28 14:49:27 +0100

    Create form for adding and saving foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,11 +7,19 @@ class FoodsController < ApplicationController
   def create
     @users = current_user
     @foods = Food.new(food_params)
+    if @food.save
+      redirect_to recipe_foods_path, notice: 'Food was successfully created.'
+    else
+      render :new, alert: 'Food was not created.'
+    end
+  end
 
+  def edit
+    
   end
 
   private 
   def food_params
-    params.require(:food).permit(:name, :measurement_unit, :price, :quantity current_user.id)
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, current_user.id)
   end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,5 @@
 class Food < ApplicationRecord
   belongs_to :user
-  has_many :recipe_foods, foreign_key: 'food_id', dependent: :destroy
   has_many :recipes, through: :recipe_foods
 
   validates :name, presence: true, length: { maximum: 100 }
