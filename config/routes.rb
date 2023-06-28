566295092f70fe573901898fe2ba3c6e412fa84e
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-06-28 08:46:01 +0300

    Setup views for recipe view, edit and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  resources :recipes
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_scope :user do 
+    authenticated :user do 
+      root 'users#index', as: :authenticated_root 
+    end
+    unauthenticated do 
+      root 'devise/sessions#new', as: :unauthenticated_root 
+    end 
+  end
 
   # Defines the root path route ("/")
-  # root "articles#index"
-
-  root to: "home#index"
+  root "users#index"
 end
