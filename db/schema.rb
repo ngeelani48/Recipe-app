f4fdb3c05e4fb948829d3a56c0b89f88899671f8
lawrahkonwea <lawrahcollections@gmail.com>
2023-06-28 12:35:22 +0100

    Migrate foods into Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_28_043209) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_28_094200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.float "price"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
